c3c4ec02b86efc2f343b90a1d98bdd8f551d4b42
Onlu <onlu.contact@gmail.com>
2025-10-29 00:16:18 +0200

    Turned form structure logic extracted from FromEditor into a custom hook

---
+++ src/pages/FormEditor/hooks/useFormStructure.ts
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+import { FormStructure, Section } from "../context/FormStructureContext";
+import { EMPTY_FORM } from "../context/constants";
+import { texts } from "../../../utils/texts";
+
+function yieldFormStructure(form: object) {
+  return form as FormStructure; // TODO change to actual logic that translates form json to form structure
+}
+
+function useFormStructure(editedForm?: object) { //TODO consider making singleton
+  const [formStructure, setFormStructure] = useState<FormStructure>(editedForm ? yieldFormStructure(editedForm) : { ...EMPTY_FORM });
+
+  useEffect(() => {
+    console.log(formStructure.sections);
+  }, [formStructure.sections]);
+
+  const appendSection = useCallback(() => {
+    setFormStructure((prev) => {
+      const newSectionId = `section_${Date.now()}`;
+      const newSection: Section = {
+        title: texts.heb.undefinedSection,
+        order: Object.keys(prev.sections).length,
+        collapsed: false,
+        fieldIds: [],
+      };
+
+      return {
+        ...prev,
+        sections: {
+          ...prev.sections,
+          [newSectionId]: newSection,
+        },
+      };
+    });
+  }, [setFormStructure]);
+
+  return {
+    formStructure,
+    appendSection,
+  };
+}
+
+export { useFormStructure };
--- src/pages/FormEditor/index.tsx
+++ src/pages/FormEditor/index.tsx
@@ -2,12 +2,11 @@ import styles from "./style.module.css";
 import { FormEditorHeader } from "./FormEditorHeader";
 import { FormSandbox } from "./FormSandbox";
 import { FormEditorMode } from "./context/FormEditorContext";
-import { useCallback, useEffect, useState } from "react";
-import { DEFAULT_DRAGGING_STATE, EMPTY_FORM } from "./context/constants";
+import { useState } from "react";
+import { DEFAULT_DRAGGING_STATE } from "./context/constants";
 import { FormContextProvider } from "./context/FormContextProvider";
-import { FormStructure, Section } from "./context/FormStructureContext";
 import { DraggingState } from "./context/FormSandboxContext";
-import { texts } from "../../utils/texts";
+import { useFormStructure } from "./hooks/useFormStructure";
 
 interface EditorProps {
   mode: FormEditorMode;
@@ -26,37 +25,9 @@ interface CreateModeProps extends EditorProps {
 
 type Props = CreateModeProps | EditModeProps;
 
-function yieldFormStructure(form: object) {
-  return form as FormStructure; // TODO change to actual logic that translates form json to form structure
-}
-
 function FormEditor({ mode, editedForm }: Props) {
-  const [formStructure, setFormStructure] = useState<FormStructure>(editedForm ? yieldFormStructure(editedForm) : { ...EMPTY_FORM });
+  const { formStructure, appendSection } = useFormStructure(editedForm);
   const [draggingState, setDraggingState] = useState<DraggingState>({ ...DEFAULT_DRAGGING_STATE });
-
-  useEffect(() => {
-    console.log(formStructure.sections);
-  }, [formStructure.sections]);
-
-  const appendSection = useCallback(() => {
-    setFormStructure((prev) => {
-      const newSectionId = `section_${Date.now()}`;
-      const newSection: Section = {
-        title: texts.heb.undefinedSection,
-        order: Object.keys(prev.sections).length,
-        collapsed: false,
-        fieldIds: [],
-      };
-
-      return {
-        ...prev,
-        sections: {
-          ...prev.sections,
-          [newSectionId]: newSection,
-        },
-      };
-    });
-  }, [setFormStructure]);
 
   return (
     <div className={styles.editorContainer}>
