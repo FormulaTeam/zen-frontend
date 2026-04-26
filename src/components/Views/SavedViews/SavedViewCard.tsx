5abaf1d432f7f4419aaa45d519f1fa4f5f781cca
raskinbarak <barak.raskin@gmail.com>
2026-04-26 17:17:01 +0300

    feat(responses): 🚀 complete Responses Views synchronization and UI modernization

--- src/components/Views/SavedViews/SavedViewCard.tsx
+++ src/components/Views/SavedViews/SavedViewCard.tsx
@@ -13,6 +13,8 @@ import {
   ViewChipsContainer,
 } from "../ViewManager/styled";
 import { ResponsesView } from "../../../types/interfaces/tableViews.types";
+import { useState } from "react";
+import BasePopup from "../../BasePopup/BasePopup";
 
 enum HebrewTitles {
   EDIT_VIEW = "עריכת תצוגה",
@@ -20,6 +22,9 @@ enum HebrewTitles {
   CREATED_BY = "נוצרה על ידי",
   DEFAULT = "ברירת מחדל",
   PUBLIC_VIEW = "תצוגה ציבורית",
+  DELETE_CONFIRM_TITLE = "מחיקת תצוגה",
+  DELETE_CONFIRM_CANCEL = "ביטול",
+  DELETE_CONFIRM_APPROVE = "מחיקה",
 }
 
 interface SavedViewCardProps {
@@ -41,76 +46,103 @@ export function SavedViewCard({
   currentViewId,
   setCurrentViewId,
 }: SavedViewCardProps) {
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
   const canEdit = canEditOrDeleteView(view);
 
   const handleLoad = () => onLoadView(view);
   const stop = (e: React.MouseEvent) => e.stopPropagation();
 
+  const openDeletePopup = (e: React.MouseEvent) => {
+    stop(e);
+    setIsDeletePopupOpen(true);
+  };
+
+  const closeDeletePopup = () => {
+    setIsDeletePopupOpen(false);
+  };
+
+  const handleDelete = () => {
+    onDeleteView?.(view);
+    closeDeletePopup();
+  };
+
   return (
-    <ViewCard
-      role="button"
-      tabIndex={0}
-      onClick={() => {
-        handleLoad();
-        setCurrentViewId && setCurrentViewId(view.id ?? null);
-      }}
-      $isSelected={currentViewId === view.id}>
-      <ViewCardContent>
-        <ViewCardActions>
-          <ViewCardInfo>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <ViewNameTypography variant="body1" $isDefault={view.isDefault}>
-                {view.name}
-              </ViewNameTypography>
-              {view.isPublic && (
-                <Tooltip title={HebrewTitles.PUBLIC_VIEW}>
-                  <PublicIcon fontSize="small" />
-                </Tooltip>
-              )}
-              {view.isDefault && <Chip label={HebrewTitles.DEFAULT} size="small"></Chip>}
-            </Stack>
+    <>
+      <ViewCard
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          handleLoad();
+          setCurrentViewId && setCurrentViewId(view.id ?? null);
+        }}
+        $isSelected={currentViewId === view.id}>
+        <ViewCardContent>
+          <ViewCardActions>
+            <ViewCardInfo>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <ViewNameTypography variant="body1" $isDefault={view.isDefault}>
+                  {view.name}
+                </ViewNameTypography>
+                {view.isPublic && (
+                  <Tooltip title={HebrewTitles.PUBLIC_VIEW}>
+                    <PublicIcon fontSize="small" />
+                  </Tooltip>
+                )}
+                {view.isDefault && <Chip label={HebrewTitles.DEFAULT} size="small"></Chip>}
+              </Stack>
 
-            <ViewChipsContainer></ViewChipsContainer>
+              <ViewChipsContainer></ViewChipsContainer>
 
-            <Typography variant="caption" color="text.secondary">
-              {HebrewTitles.CREATED_BY}{" "}
-              {view.createdByName ||
-                (typeof view.createdBy === "object" ? view.createdBy.name : view.createdBy)}
-            </Typography>
-          </ViewCardInfo>
+              <Typography variant="caption" color="text.secondary">
+                {HebrewTitles.CREATED_BY}{" "}
+                {view.createdByName ||
+                  (typeof view.createdBy === "object" ? view.createdBy.name : view.createdBy)}
+              </Typography>
+            </ViewCardInfo>
 
-          {canEdit && (
-            <ViewCardButtons>
-              {onEditView && (
-                <Tooltip title={HebrewTitles.EDIT_VIEW}>
-                  <IconButton
-                    size="small"
-                    onClick={(e) => {
-                      stop(e);
-                      onEditView(view);
-                    }}>
-                    <Edit />
-                  </IconButton>
-                </Tooltip>
-              )}
+            {canEdit && (
+              <ViewCardButtons>
+                {onEditView && (
+                  <Tooltip title={HebrewTitles.EDIT_VIEW}>
+                    <IconButton
+                      size="small"
+                      onClick={(e) => {
+                        stop(e);
+                        onEditView(view);
+                      }}>
+                      <Edit />
+                    </IconButton>
+                  </Tooltip>
+                )}
 
-              {onDeleteView && (
-                <Tooltip title={HebrewTitles.DELETE_VIEW}>
-                  <IconButton
-                    size="small"
-                    color="error"
-                    onClick={(e) => {
-                      stop(e);
-                      onDeleteView(view);
-                    }}>
-                    <DeleteIcon />
-                  </IconButton>
-                </Tooltip>
-              )}
-            </ViewCardButtons>
-          )}
-        </ViewCardActions>
-      </ViewCardContent>
-    </ViewCard>
+                {onDeleteView && (
+                  <Tooltip title={HebrewTitles.DELETE_VIEW}>
+                    <IconButton size="small" color="error" onClick={openDeletePopup}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
+                )}
+              </ViewCardButtons>
+            )}
+          </ViewCardActions>
+        </ViewCardContent>
+      </ViewCard>
+
+      <BasePopup
+        open={isDeletePopupOpen}
+        onClose={closeDeletePopup}
+        title={HebrewTitles.DELETE_CONFIRM_TITLE}
+        content={`האם אתה בטוח שברצונך למחוק את תצוגת ${view.name}?`}
+        mainButton={{
+          text: HebrewTitles.DELETE_CONFIRM_APPROVE,
+          onClick: handleDelete,
+          color: "error",
+        }}
+        cancelButton={{
+          text: HebrewTitles.DELETE_CONFIRM_CANCEL,
+          onClick: closeDeletePopup,
+        }}
+      />
+    </>
   );
 }
