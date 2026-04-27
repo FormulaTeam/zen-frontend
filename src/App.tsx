6693a1a0e4021ed8fee1bf5f9e4d2441462d9960
raskinbarak <barak.raskin@gmail.com>
2026-04-27 13:10:46 +0300

    feat(dx): 🗑️ remove TanStack Query Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./contexts/AuthContext";
 import { FormChangesProvider } from "./contexts/FormChangesContext";
 import { SuperAdminProvider } from "./contexts/SuperAdminContext";
@@ -10,7 +9,6 @@ import { muiLicenseKey } from "./muiLicense";
 
 function App() {
   LicenseInfo.setLicenseKey(muiLicenseKey);
-  const isProd = (window as any).RUNTIME_ENV?.REACT_APP_IS_PROD ?? false;
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -21,8 +19,6 @@ function App() {
           </FormChangesProvider>
         </SuperAdminProvider>
       </AuthProvider>
-      {/* React Query Devtools - only shows when not in production */}
-      {!isProd && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 }
