e048d64659505b0695af8ab580506679e32d3c3c
revital2002 <revitalf512@gmail.com>
2026-04-27 15:25:11 +0300

    fix: options and forms inside forms works well

--- src/pages/FormEditor/FormSandbox/FormStructure/FormFieldElement/ExtraElement/elements/OptionsFieldExtra/FormFieldResponsesOptions/index.tsx
+++ src/pages/FormEditor/FormSandbox/FormStructure/FormFieldElement/ExtraElement/elements/OptionsFieldExtra/FormFieldResponsesOptions/index.tsx
@@ -1,31 +1,18 @@
 import { OptionsSource } from "@pages/FormEditor/schemas/fields/optionsSchema";
 import React, { useEffect, useState, useMemo } from "react";
-import {
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  MenuItem,
-  Select,
-  Box,
-  CircularProgress,
-  styled,
-  Autocomplete,
-  TextField,
-  Tooltip,
-} from "@mui/material";
+import { FormControl, CircularProgress, Autocomplete, TextField, Tooltip } from "@mui/material";
 import { ExtraElementProps } from "../../../index";
 import { OptionsFieldTypeId, SpecificOptions, SpecificOptionsErrors } from "../index";
-import { useGetForm, useGetLinkableForms } from "@api/formsApi";
+import { useGetForm } from "@api/formsApi";
+import { useGetFormsData } from "@hooks/useGetFormsData";
+import { formsScopeOption } from "@src/types/enums/filtersAndSorts.enum";
 import { FormSectionDto, FormFieldDto } from "@src/types/shared";
 import { useFormStructureContext } from "@pages/FormEditor/context/FormStructureContext";
 import { FormOption } from "@utils/interfaces";
 import { LoaderContainer, Container, FieldControl } from "./styled";
-import { useParams } from "react-router-dom";
-import { FieldTypeIds } from "@utils/interfaces";
-import * as Gear from "formula-gear";
+import { fieldType } from "formula-gear";
 
-interface Props
-  extends Omit<ExtraElementProps<OptionsFieldTypeId>, "extra" | "validationErrors" | "disabled"> {
+interface Props extends Omit<ExtraElementProps<OptionsFieldTypeId>, "extra" | "validationErrors" | "disabled"> {
   options: SpecificOptions<OptionsSource.FORM_FIELD_RESPONSES>;
   validationErrors: SpecificOptionsErrors<OptionsSource.FORM_FIELD_RESPONSES> | undefined;
 }
@@ -35,67 +22,61 @@ interface ValidField {
   displayName: string;
 }
 
-// Helper to bypass static analysis of bundlers which might have stale cache
-const getGearConstant = (key: string) => {
-  const g = Gear as any;
-  return g[key];
-};
-
 function FormFieldResponsesOptions(props: Props) {
-  const { options = { formId: "", fieldId: "" }, validationErrors, onChange } = props;
+  const {
+    options = { formId: "", fieldId: "" },
+    validationErrors,
+    onChange,
+  } = props;
 
   const { formStructure } = useFormStructureContext();
-  const { id: urlFormId } = useParams();
   const [searchText, setSearchText] = useState("");
   const [fieldTouchAttempted, setFieldTouchAttempted] = useState(false);
-
-  const currentFormId = formStructure?.metadata?.id || urlFormId;
-
-  const { data: allForms = [], isLoading: isLoadingForms } = useGetLinkableForms({
-    formId: currentFormId ? currentFormId.toString() : "",
-    search: searchText || undefined,
+  const { formsData: allForms, isLoading: isLoadingForms } = useGetFormsData({
+    searchQuery: searchText.length >= 2 ? searchText : undefined,
+    scope: formsScopeOption.AccessibleForms,
+    enabled: searchText.length >= 2,
   });
 
-  const availableForms = useMemo(() => {
-    return allForms;
-  }, [allForms]);
+  const availableForms = useMemo<FormOption[]>(() => {
+    const list = formStructure?.metadata?.id
+      ? allForms.filter((form) => form.id !== formStructure.metadata.id)
+      : allForms;
+
+    return list.map((form) => ({ id: form.id.toString(), name: form.name }));
+  }, [allForms, formStructure?.metadata?.id]);
 
   const { data: initialForm, isLoading: isInitializing } = useGetForm({
     formId: options?.formId ? options.formId : undefined,
   });
 
-  const selectedFormOption = useMemo(() => {
+  const selectedFormOption = useMemo<FormOption | null>(() => {
     if (!initialForm) return null;
-    return { id: initialForm.id, name: initialForm.name };
+    return { id: initialForm.id.toString(), name: initialForm.name };
   }, [initialForm]);
 
   const availableFields = useMemo<ValidField[]>(() => {
     if (!initialForm) return [];
 
-    const fields = (initialForm?.sections || []).reduce(
-      (acc: FormFieldDto[], section: FormSectionDto) => {
-        return [...acc, ...(section.fields || [])];
-      },
-      [],
-    );
+    const fields = (initialForm?.sections || []).reduce((acc: FormFieldDto[], section: FormSectionDto) => {
+      return [...acc, ...(section.fields || [])];
+    }, []);
 
-    // Use bracket notation to bypass static analysis of bundler which might have stale cache
-    const gearComparableFieldTypes = getGearConstant("comparable" + "FieldTypes") || [
-      Gear.fieldType.Number,
-      Gear.fieldType.ShortText,
-      Gear.fieldType.LongText,
-      Gear.fieldType.Date,
-      Gear.fieldType.Time,
-      Gear.fieldType.Location,
+    const allowedTypes = [
+      fieldType.Number,
+      fieldType.ShortText,
+      fieldType.LongText,
+      fieldType.Date,
+      fieldType.Time,
+      fieldType.Location,
+      fieldType.Link,
     ];
 
-    const filtered = fields.filter((field) =>
-      (gearComparableFieldTypes as number[]).includes(field.fieldType),
-    );
+    const filtered = fields.filter((field) => allowedTypes.some((type) => type === field.fieldType));
 
     return filtered.map((field: FormFieldDto) => ({
       id: field.id.toString(),
-      displayName: field.displayName,
+      displayName: field.displayName
     }));
   }, [initialForm]);
 
@@ -120,23 +101,18 @@ function FormFieldResponsesOptions(props: Props) {
     <FormControl error={!!validationErrors?.properties?.formId}>
       <Autocomplete
         options={availableForms}
-        getOptionLabel={(option: any) => option?.name || ""}
+        getOptionLabel={(option) => option?.name || ""}
         value={selectedFormOption}
-        inputValue={searchText}
         loading={isLoadingForms}
         loadingText="מחפש..."
-        noOptionsText="לא נמצאו תוצאות"
-        onInputChange={(_, newInputValue, reason) => {
-          if (reason === "input") {
-            setSearchText(newInputValue);
-          } else if (reason === "clear") {
-            setSearchText("");
-          }
+        noOptionsText={searchText.length < 2 ? "יש להזין לפחות 2 תווים" : "לא נמצאו תוצאות"}
+        onInputChange={(_, newInputValue) => {
+          setSearchText(newInputValue);
         }}
-        onChange={(_, newValue: any) => {
+        onChange={(_, newValue) => {
           onChange({
             source: OptionsSource.FORM_FIELD_RESPONSES,
-            options: { ...options, formId: newValue ? newValue.id.toString() : "", fieldId: "" },
+            options: { ...options, formId: newValue ? newValue.id.toString() : "", fieldId: "" }
           });
         }}
         isOptionEqualToValue={(option, value) => option?.id === value?.id}
@@ -164,8 +140,8 @@ function FormFieldResponsesOptions(props: Props) {
 
   const fieldSelect: JSX.Element = (
     <FieldControl error={!!validationErrors?.properties?.fieldId}>
-      <Tooltip title={!options?.formId ? "יש לבחור טופס" : ""}>
-        <span style={{ display: "block" }}>
+      <Tooltip title={!options?.formId ? 'יש לבחור טופס' : ''}>
+        <span style={{ display: 'block' }}>
           <Autocomplete
             options={availableFields}
             getOptionLabel={(option) => option.displayName || ""}
@@ -174,18 +150,10 @@ function FormFieldResponsesOptions(props: Props) {
             onChange={(_, newValue) => {
               onChange({
                 source: OptionsSource.FORM_FIELD_RESPONSES,
-                options: { ...options, fieldId: newValue ? newValue.id : "" },
+                options: { ...options, fieldId: newValue ? newValue.id : "" }
               });
             }}
-            noOptionsText={
-              !options?.formId
-                ? fieldTouchAttempted
-                  ? "יש לבחור טופס"
-                  : ""
-                : availableFields.length
-                  ? "לא נמצאו תוצאות"
-                  : "אין שדות זמינים"
-            }
+            noOptionsText={!options?.formId ? (fieldTouchAttempted ? 'יש לבחור טופס' : '') : (availableFields.length ? 'לא נמצאו תוצאות' : 'אין שדות זמינים')}
             disabled={!options?.formId}
             renderInput={(params) => (
               <TextField
