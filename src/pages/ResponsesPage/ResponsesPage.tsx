47066cdcfd3c595b2c33939ceddf8a06229679ca
raskinbarak <barak.raskin@gmail.com>
2026-04-27 18:08:59 +0300

    fix(responses): 📊 ensure initial load and resets default to index desc

--- src/pages/ResponsesPage/ResponsesPage.tsx
+++ src/pages/ResponsesPage/ResponsesPage.tsx
@@ -20,6 +20,7 @@ import { useResponsesViews } from "./hooks/useResponsesViews";
 import { useFormStore } from "./stores/form.store";
 import { ActionsRow, CenteredBox, MainContentWrapper, PageWrapper, TopSection } from "./styled";
 import { FormDto, FormFieldDto, UserPersonalDto } from "../../types/shared";
+import { IOrderBy } from "@src/types/enums/filtersAndSorts.enum";
 
 type ResponsePageRow = GridRowModel & {
   id: string | number;
@@ -113,6 +114,8 @@ const ResponsesPageContent = (): JSX.Element => {
         before: undefined,
         after: undefined,
         pageNumber: 1,
+        sortBy: filter?.sortBy || "meta:index",
+        orderBy: filter?.orderBy || IOrderBy.DESC,
       });
     }, 500);
     return () => clearTimeout(timer);
