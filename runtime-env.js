04fcd6966ad37a6fa3d48d0c9e31cb14a7d0cb36
NOren <noren25@dev.sky320.com>
2026-05-10 12:06:51 +0300

    dockerfile

--- runtime-env.js
+++ runtime-env.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const envVars = {
   REACT_APP_API_URL: process.env.REACT_APP_API_URL || "http://localhost:3000",
+  REACT_APP_KEYCLOAK_URL: process.env.REACT_APP_KEYCLOAK_URL || "",
   REACT_MAX_PAYLOAD_SIZE_MB: process.env.REACT_MAX_PAYLOAD_SIZE_MB || 1,
   IS_PROD: process.env.IS_PROD || false,
 };
