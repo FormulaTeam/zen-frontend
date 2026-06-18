5dbf1d00ac0736bd52932c4dcfd715a1484f204e
GaliZvi <galizvi72@gmail.com>
2026-06-18 16:28:45 +0300

    refactor(hooks): 🛠️ Improve formatting and structure in useConfirmDeleteExistingField

--- src/pages/FormEditor/hooks/useConfirmDeleteExistingField.tsx
+++ src/pages/FormEditor/hooks/useConfirmDeleteExistingField.tsx
@@ -2,26 +2,37 @@ import React, { useState } from "react";
 import ConfirmDeleteDialog from "@src/components/BasePopup/ConfirmDeleteDialog";
 
 export function useConfirmDeleteExistingField() {
-    const [confirmOpen, setConfirmOpen] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
+  const [onConfirm, setOnConfirm] = useState<null | (() => void)>(null);
 
-    const [onConfirm, setOnConfirm] = useState<null | (() => void)>(null);
+  const requestConfirm = (onConfirmAction: () => void) => {
+    setOnConfirm(() => onConfirmAction);
+    setConfirmOpen(true);
+  };
 
-    const requestConfirm = (onConfirmAction: () => void) => {
-        setOnConfirm(() => onConfirmAction);
-        setConfirmOpen(true);
-    };
+  const handleConfirm = () => {
+    onConfirm?.();
+    setConfirmOpen(false);
+    setOnConfirm(null);
+  };
 
-    const ConfirmDialog = confirmOpen && onConfirm ? (
-        <ConfirmDeleteDialog
-            open={confirmOpen}
-            title="אזהרה"
-            message="לתשומת לבך! ייתכן ושדה זה מכיל תוכן כחלק מהתגובות לטופס. לכן, מחיקת השדה תוביל למחיקה והסרת הנתונים מהטופס ללא יכולת שחזור. האם למחוק שדה זה מהטופס?"
-            onConfirm={onConfirm}
-            onClose={() => setConfirmOpen(false)}
-            confirmText="מחק שדה"
-            cancelText="בטל"
-        />
+  const handleClose = () => {
+    setConfirmOpen(false);
+    setOnConfirm(null);
+  };
+
+  const ConfirmDialog =
+    confirmOpen && onConfirm ? (
+      <ConfirmDeleteDialog
+        open={confirmOpen}
+        title="אזהרה"
+        message="לתשומת לבך! ייתכן ושדה זה מכיל תוכן כחלק מהתגובות לטופס. לכן, מחיקת השדה תוביל למחיקה והסרת הנתונים מהטופס ללא יכולת שחזור. האם למחוק שדה זה מהטופס?"
+        onConfirm={handleConfirm}
+        onClose={handleClose}
+        confirmText="מחק שדה"
+        cancelText="בטל"
+      />
     ) : null;
 
-    return { requestConfirm, ConfirmDialog };
+  return { requestConfirm, ConfirmDialog };
 }
